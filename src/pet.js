f559aaffecc5212f37b2d4a87b7832c6f0650150
Rayhan Elbeera <raybeera2014@gmail.com>
2022-10-29 11:03:47 +0100

    getting older

--- src/pet.js
+++ src/pet.js
@@ -1,4 +1,21 @@
 // pet constructor function
-function Pet() { }
+//NOTE:constructor functions are functions, and are not objects themselves.
+//When using a constructor function, we have to use the keyword this
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+    this.growUp = function () {
+        this.age += 1;
+    }
+}
+
+const pet = new Pet('Fido');
+console.log(pet);
+
+
+
+
+
+
 
 module.exports = Pet; //file that been exposed (make it available to use outside the file it written init)
